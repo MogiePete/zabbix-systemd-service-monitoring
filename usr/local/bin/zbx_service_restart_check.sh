6b21a8a9ce6efc421506d710d191021199ab9c38
Gareth Brown <gaz@gbmail.me>
2021-02-20 11:44:03 +0000

    revert formatting back to author's style

--- usr/local/bin/zbx_service_restart_check.sh
+++ usr/local/bin/zbx_service_restart_check.sh
@@ -6,9 +6,10 @@ now=$(date +%s)
 # Don't alert if the server has just been restarted
 uptime=$(date +%s -d "$(systemctl show --value -p ActiveEnterTimestamp sysinit.target)")
 if [[ $(( now - uptime)) -lt 180 ]]; then
-  echo 0
+        echo 0
 else
-  service_start=$(systemctl show --value -p ActiveEnterTimestamp "$service")
-  service_start_as_epoch=$(date -d "$service_start" +%s)
-  [[ $(( now - service_start_as_epoch )) -lt 180 ]] && echo 1 || echo 0
+        service_start=$(systemctl show --value -p ActiveEnterTimestamp "$service")
+        service_start_as_epoch=$(date -d "$service_start" +%s)
+
+        [[ $(( now - service_start_as_epoch )) -lt 180 ]] && echo 1 || echo 0
 fi
